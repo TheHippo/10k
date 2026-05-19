e8555a6fea478369cbecdfdeecad79d8cec967a5
Philipp Klose <me@thehippo.de>
2026-05-20 01:18:38 +0200

    Added DaisyUI

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="mx-auto w-full max-w-screen-xl px-4 sm:px-6 lg:px-8">
     <NuxtPage />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false,
+  vite: {
+    plugins: [tailwindcss() as any],
+  },
+  css: ['~/assets/css/main.css'],
 })
